84fa5aed3673d5398392e09a66f2f547f1624037
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-09-09 21:34:37 +0300

    feat(core): upgrade prisma to version 3

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,7 +1,7 @@
 /* Instruments */
 import { Resolver } from '../types';
 
-const links: Resolver<unknown, { id: number }> = (parent, _, ctx) => {
+const links: Resolver<{ id: number }> = (parent, _, ctx) => {
     return ctx.prisma.user.findUnique({ where: { id: parent.id } }).links();
 };
 
